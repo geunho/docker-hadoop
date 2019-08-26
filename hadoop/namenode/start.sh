a4667e2069af271d5f26e9cdbaf1210ea45fc36a
GeunhoKim <geunho.dev@gmail.com>
2019-08-26 17:10:06 +0900

    Fix namenode startup error

--- hadoop/namenode/start.sh
+++ hadoop/namenode/start.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 NAME_DIR=$1
+echo $NAME_DIR
 
-if [ "`ls -A $NAME_DIR`" == "" ]; then
+if [ "$(ls -A $NAME_DIR)" ]; then
+  echo "NameNode is already formatted."
+else
+  echo "Format NameNode."
   $HADOOP_PREFIX/bin/hdfs --config $HADOOP_CONF_DIR namenode -format
 fi
 
