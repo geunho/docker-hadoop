34c043767c8b5ec5843a5d802fc49ad8f12f2144
GeunhoKim <geunho.dev@gmail.com>
2019-09-02 17:31:23 +0900

    On work to build historyserver
    - Error occurs on application history page
    ```
    Aggregation is not enabled. Try the nodemanager at nodemanager03:38879
    Or see application log at http://nodemanager03:38879/node/application/application_1567412700147_0002
    ```

--- hadoop/build.sh
+++ hadoop/build.sh
@@ -4,18 +4,20 @@ BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 docker_build() {
     target=$1
+    build_options=$2
 
     if [ "$(ls -A $BASE_DIR/$target)" ]; then
         echo "Build $target docker image..."
         cd $BASE_DIR/$target
-        docker build -t geunho/hadoop-$target:2.9.2 .
+        docker build $build_options -t geunho/hadoop-$target:2.9.2 .
     else
         echo "Target error: $1 does not exiss. (base|namenode|datanode|resourcemanager|nodemanager|proxy)"
     fi
 }
 
 docker_build base
-docker_build namenode
-docker_build datanode
-docker_build resourcemanager
-docker_build nodemanager
+docker_build namenode --no-cache
+docker_build datanode --no-cache
+docker_build resourcemanager --no-cache
+docker_build nodemanager --no-cache
+docker_build historyserver --no-cache
